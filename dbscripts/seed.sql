fd2a2c727e51589166233770d35badcbfad51d82
Nattaaek Wattanuyan <w.nattaaek@yahoo.com>
2020-08-13 22:37:03 +0700

    init

---
+++ dbscripts/seed.sql
@@ -0,0 +1,23 @@
+\connect mdb
+CREATE TABLE tweet
+(
+ id serial PRIMARY KEY,
+ user VARCHAR (50) NOT NULL,
+ tweet_message VARCHAR (100) NOT NULL
+);
+ALTER TABLE "tweet" OWNER TO sa;
+Insert into tweet(user,tweet_message) values( 'K k','I thought topson was screaming yahoo');
+Insert into tweet(user,tweet_message) values( 'Rafael Rambaho','Topias? Can u make a challenge, like jakiro mid with alien items?');
+Insert into tweet(user,tweet_message) values( 'Nino Jabra','even topson is not safe from the russians');
+Insert into tweet(user,tweet_message) values( 'EmbraceMe','Gorphling vs. Topiaphling?');
+Insert into tweet(user,tweet_message) values( 'Awaken','в каждом выпуске об санлайта ноги вытирает xD');
+Insert into tweet(user,tweet_message) values( 'CrossGames','Is that PA really bamboe?');
+Insert into tweet(user,tweet_message) values( 'Md. Ifekhar Hossain Rifat','Miracle playing trent? That\'s rare!');
+Insert into tweet(user,tweet_message) values( 'Stefan','Nice');
+Insert into tweet(user,tweet_message) values( 'jeremiah mesa','Subscribed');
+Insert into tweet(user,tweet_message) values( 'Poonyaporn Srithanabout','Senpai notice me');
+Insert into tweet(user,tweet_message) values( 'Joseph Aldo Soto Aparcana','The best');
+Insert into tweet(user,tweet_message) values( 'Jitu Vasista','Nice');
+Insert into tweet(user,tweet_message) values( 'AlessFD','elwono');
+Insert into tweet(user,tweet_message) values( 'Epic Scorpion','Fuck you topsan');
+Insert into tweet(user,tweet_message) values( 'Jigme Barphungpa','Plz coach me');
